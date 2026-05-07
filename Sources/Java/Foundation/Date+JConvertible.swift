0c229d5b8e748461fbbb3d6a7b5224b15c2c85f3
Isaac Paul <izackp@gmail.com>
2026-05-07 09:17:35 -0400

    Fix Date bridging precision + add JParameterConvertible
    - fromJavaObject: treat ms as ms (was treating as seconds, off by 1000x).
    - toJavaObject: round milliseconds via *1000 so sub-second precision survives.
    - Add JParameterConvertible so Date can be used as @jvm func param.
    
    Automated-By: claude-opus-4-7

--- Sources/Java/Foundation/Date+JConvertible.swift
+++ Sources/Java/Foundation/Date+JConvertible.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Date: JObjectConvertible, JNullInitializable {
+extension Date: JObjectConvertible, JNullInitializable, JParameterConvertible {
   private enum __JClass__ {
     static let name = "java/util/Date"
     static let shared = {
@@ -15,13 +15,12 @@ extension Date: JObjectConvertible, JNullInitializable {
   public nonisolated static var javaClass: JClass { __JClass__.shared }
 
   public static func fromJavaObject(_ obj: JavaObject) -> Date {
-    let jtime: Int64 = JObject(obj).call(method: "getTime")
-    return Date(timeIntervalSince1970: TimeInterval(jtime))
+    let ms: Int64 = JObject(obj).call(method: "getTime")
+    return Date(timeIntervalSince1970: Double(ms) / 1000.0)
   }
 
   public func toJavaObject() -> JavaObject? {
-    // from seconds since 1970 to milliseconds
-    return Date.javaClass.create(Int64(timeIntervalSince1970) * 1000)
+    let ms = Int64((timeIntervalSince1970 * 1000.0).rounded())
+    return Date.javaClass.create(ms)
   }
 }
-
